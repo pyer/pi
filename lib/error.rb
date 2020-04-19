f3cd45adfe23ca644a7df31399288afa9a939a4f
Pierre BAZONNARD <pierre.bazonnard@gmail.com>
2020-04-19 18:16:43 +0200

    + images folder

--- lib/error.rb
+++ lib/error.rb
@@ -2,7 +2,7 @@ class Error
 
   def self.error404
     status = 404
-    body = File.read('themes/default/error404.jpg') 
+    body = File.read('images/error404.jpg')
     length = body.bytesize
     headers = { 'Content-Type' => 'image/jpeg', 'Content-Length' => length.to_s }
     return [status, headers, [body]]
